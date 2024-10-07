1c45dd687d65389db5e72c1638dda0dc1464d391
alex <alexnasiali45@gmail.com>
2024-10-07 09:55:44 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 document.addEventListener("DOMContentLoaded",() => {
-const form = document.querySelector("form");
-form.addEventListener("submit",(e) => {
-    e.preventDefault();
-    const tasks = document.getElementById("create-task-form").value();
-    let listContainer = document.getElementById("list");
-    form.onsubmit = (e) =>{
-        e.preventDefault();
-        let newTask = document.getElementById("new-task-descption").value;
-        let colors = document.getElementById("colors").value;
-        listContainer.appendChild(listFunction(newTask,colors));
-        form.reset()
-    }
+const form = document.getElementById("create-task-form");
+const ul = document.getElementById("tasks")
+
+form.addEventListener("submit",(event) => {
+    event.preventDefault();
+    const taskInput = document.getElementById("new-task-description")
+    const task = taskInput.value
+    console.log(task)
+
+    const li = document.createElement('li')
+    li.innerText = task
+    ul.appendChild(li)
+    // form.onsubmit = (e) =>{
+    //     let newTask = document.getElementById("new-task-descption").value;
+    //     let colors = document.getElementById("colors").value;
+    //     listContainer.appendChild(listFunction(newTask,colors));
+    //     form.reset()
+    // }
     
 })
-function addToDoList(task,color){
-    let lists = document.createElement("li");
-    lists.textContent = task;
-    lists.style.color = color;
-    edit.textContent = () => "edit";
-    edit.onclick = () => {
-        let addedContent = prompt("Edit item", task);
-        if (addedContent !== "")
-        lists.textContent = addedContent;
-    lists.appendChild(del)
-    lists.appendChild(edit)
-}
-lists.appendChild(edit);
-    let handleDelete = document.createElement("button");
-    handleDelete.textContent = "x";
-    handleDelete.onclick = () => {
-        lists.remove();
-    }
-    lists.appendChild(handleDelete)
-    return lists;
-}
+// function addToDoList(task,color){
+//     let lists = document.createElement("li");
+//     lists.textContent = task;
+//     lists.style.color = color;
+//     edit.textContent = () => "edit";
+//     edit.onclick = () => {
+//         let addedContent = prompt("Edit item", task);
+//         if (addedContent !== "")
+//         lists.textContent = addedContent;
+//     lists.appendChild(del)
+//     lists.appendChild(edit)
+// }
+// lists.appendChild(edit);
+//     let handleDelete = document.createElement("button");
+//     handleDelete.textContent = "x";
+//     handleDelete.onclick = () => {
+//         lists.remove();
+//     }
+//     lists.appendChild(handleDelete)
+//     return lists;
+// }
 
 })
--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form")
+  const ul = document.getElementById("tasks")
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskInput = document.getElementById("new-task-description")
+    const colorSelector = document.getElementById('select_colors')
+    const task = taskInput.value
+    const color = colorSelector.value
+
+    const li = document.createElement("li")
+
+    const span = document.createElement("span")
+    span.innerText = task
+    li.appendChild(span)
+
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText = 'X'
+    li.appendChild(deleteButton)
+
+    const editButton = document.createElement('button')
+    editButton.innerText = 'Edit'
+    li.appendChild(editButton)
+
+
+    ul.appendChild(li)
+
+    taskInput.value = ''
+
+    deleteButton.addEventListener('click', () => {
+      ul.removeChild(li)
+    })
+
+    editButton.addEventListener('click', () => {
+     const editedText = prompt('Edit', span.textContent)
+     if (editedText) {
+      span.textContent = editedText
+     }
+    })
+
+
+
+    
+    
+  })
 });
